450333176f1d9f2e5955720dd73e8b3ee1a67dac
Ars8 <pogosov-91@mail.ru>
2023-10-06 07:01:00 +0300

    remember me part 1

--- classes/User.php
+++ classes/User.php
@@ -1,18 +1,45 @@
 <?php
 
 class User {
-    private $db, $data, $session_name;
+    private $db, $data, $session_name, $isLoggedIn;
 
-    public function __construct() {
+    public function __construct($user = null) {
         $this->db = Database::getInstance();
         $this->session_name = Config::get('session.user_session');
+
+        if(!$user) {
+            if(Session::exists($this->session_name)) {
+                $user = Session::get($this->session_name);
+
+                if($this->find($user)) {
+                    $this->isLoggedIn = true;
+                } else {
+                    //logout
+                }
+            }            
+        } else {
+            $this->find($user);
+        }
     }
 
     public function create($fields = []) {
         $this->db->insert('users', $fields);
     }
 
-    public function login($email = null, $password = null) {
+    public function login($email = null, $password = null, $remember = false) {
+        if(!$email && !$password && $this->exists()) {
+            Session::put($this->session_name, $this->data()->id);
+        } else {
+            $user = $this->find($email);
+            if($user) {
+
+            }
+        }
+
+
+
+
+
         if($email) {
 
             $user = $this->find($email);
@@ -26,8 +53,13 @@ class User {
         return false;
     }
 
-    public function find($email = null) {
-        $this->data = $this->db->get('users', ['email', '=', $email])->first();
+    public function find($value = null) {
+        if(is_numeric($value)) {
+            $this->data = $this->db->get('users', ['id', '=', $value])->first();
+        } else {
+            $this->data = $this->db->get('users', ['email', '=', $value])->first();
+        }
+        
         if($this->data) {
             return true;
         }
@@ -37,5 +69,13 @@ class User {
 
     public function data() {
         return $this->data;
+    }
+
+    public function isLoggedIn() {
+        return $this->isLoggedIn;
+    }
+
+    public function logout() {
+        return Session::delete($this->session_name);
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 require_once 'init.php';
 
-var_dump(Session::get(Config::get('session.user_session')));
+$user = new User;
+$anotherUser = new User(7);
+if($user->isLoggedIn()) {
+    echo "Hi, <a href='#'>{$user->data()->username}</a>";
+    echo "<p><a href='logout.php'>Logout</a></p>";
+} else {
+    echo "<a href='login.php'>Login</a> or <a href='register.php'>Register</a>";
+}
