1682f99f1f8c9138fb9bf729b6f9d2cad5786572
Ars8 <pogosov-91@mail.ru>
2023-10-06 18:22:03 +0300

    remember me part 2

--- classes/User.php
+++ classes/User.php
@@ -1,7 +1,7 @@
 <?php
 
 class User {
-    private $db, $data, $session_name, $isLoggedIn;
+    private $db, $data, $session_name, $isLoggedIn, $cookieName;
 
     public function __construct($user = null) {
         $this->db = Database::getInstance();
@@ -32,20 +32,24 @@ class User {
         } else {
             $user = $this->find($email);
             if($user) {
-
-            }
-        }
-
-
-
-
-
-        if($email) {
-
-            $user = $this->find($email);
-            if($user) {
                 if(password_verify($password, $this->data()->password)) {
                     Session::put($this->session_name, $this->data()->id);
+
+                    if($remember) {
+                        $hash = hash('sha256', uniqid());
+
+                        $hashCheck = $this->db->get('user_sessions', ['user_id', '=', $this->data()->id]);
+
+                        if(!$hashCheck->count()) {
+                            $this->db->insert('user_session', [
+                                'user_id' => $this->data()->id,
+                                'hash' => $hash
+                            ]);
+                        } else {
+                            $hash = $hashCheck->first()->hash;
+                        }
+                        Cookie::put($this->cookieName, $hash, Config::get('cookie.cookie_expiry'));
+                    }
                     return true;
                 }
             }
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once 'init.php';
 
 $user = new User;
-$anotherUser = new User(7);
 if($user->isLoggedIn()) {
     echo "Hi, <a href='#'>{$user->data()->username}</a>";
     echo "<p><a href='logout.php'>Logout</a></p>";
